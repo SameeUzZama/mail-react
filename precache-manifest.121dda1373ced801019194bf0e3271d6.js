2c8ea5ca81530f94f2c5188f666923593392c61e
Samee Uz Zama <94825943+SameeUzZama@users.noreply.github.com>
2023-01-03 18:56:33 +0530

    Updates

---
+++ precache-manifest.121dda1373ced801019194bf0e3271d6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c1d5365c50a9a50c8b24c1b97fb363",
+    "url": "/mail-react/index.html"
+  },
+  {
+    "revision": "1862c1a51c8e3fc8d2e5",
+    "url": "/mail-react/static/css/main.16a42869.chunk.css"
+  },
+  {
+    "revision": "4a8e3004ffee36f342a9",
+    "url": "/mail-react/static/js/2.56d59546.chunk.js"
+  },
+  {
+    "revision": "7cc12235e043f862fd0ef093a135afb2",
+    "url": "/mail-react/static/js/2.56d59546.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1862c1a51c8e3fc8d2e5",
+    "url": "/mail-react/static/js/main.98e4361a.chunk.js"
+  },
+  {
+    "revision": "e0322b514d2bc4dedcbe",
+    "url": "/mail-react/static/js/runtime-main.c537934c.js"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/mail-react/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "2d0bdc8df10dee036ca3bedf6f3647c6",
+    "url": "/mail-react/static/media/OpenSans-Light.2d0bdc8d.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/mail-react/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/mail-react/static/media/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "0b6ed8720471b66c98f11bf3854fb4bb",
+    "url": "/mail-react/static/media/envelope.0b6ed872.svg"
+  },
+  {
+    "revision": "d9d021d5509f3e8f3f9ec0dfa1774a52",
+    "url": "/mail-react/static/media/nomail.d9d021d5.svg"
+  }
+]);
